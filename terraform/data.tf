60e21cf9b787a4810bdebd923779b5200286adde
Ani Shahinyan <ani.shahinyan@sada.com>
2025-10-23 17:30:56 +0400

    actionsv5 for images

--- terraform/data.tf
+++
@@ -1,7 +0,0 @@
-# data "google_iam_policy" "p4sa-secretAccessor" {
-#   binding {
-#     role = "roles/secretmanager.secretAccessor"
-#     // Here, 123456789 is the Google Cloud project number for the project that contains the connection.
-#     members = ["serviceAccount:service-123456789@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
-#   }
-# }
