9e7217b20a164c6dbad0381502dc880b7a3d382f
Ani Shahinyan <ani.shahinyan@sada.com>
2025-10-24 13:17:12 +0400

    fixing the docker commands

--- ssh-vm/iam.tf
+++ ssh-vm/iam.tf
@@ -1,10 +1,14 @@
+
 resource "google_service_account" "default" {
   account_id   = "my-custom-sa"
   display_name = "Custom SA for VM Instance"
+  depends_on = [ google_project_service.project ]
 }
+
 
 resource "google_project_iam_member" "artifact_registry_reader" {
   project = "build-project-476008"
   role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${google_service_account.default.email}"
 }
+
