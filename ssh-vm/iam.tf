91055d14ba6bfc80d977ad615bd46fb08a351585
Ani Shahinyan <ani.shahinyan@sada.com>
2025-10-24 13:19:13 +0400

    fixing the docker commands

--- ssh-vm/iam.tf
+++ ssh-vm/iam.tf
@@ -2,7 +2,6 @@
 resource "google_service_account" "default" {
   account_id   = "my-custom-sa"
   display_name = "Custom SA for VM Instance"
-  depends_on   = [google_project_service.project]
 }
 
 
